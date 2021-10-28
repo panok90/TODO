d5fb31eaf45eae5141e88b4c7a509164586d2287
PAN <cool.cyclon@yandex.ru>
2021-10-28 23:00:11 +0300

    created commands

--- users/management/commands/create_super_user.py
+++ users/management/commands/create_super_user.py
@@ -7,13 +7,13 @@ class Command(BaseCommand):
     help = 'Создание случайного пользователя'
 
     def add_arguments(self, parser):
-        parser.add_argument('-u', type=str, help='Имя пользователя')
-        parser.add_argument('-p', type=str, help='Пароль')
-        parser.add_argument('-e', type=str, help='Почта')
+        parser.add_argument('-u', '--user', type=str, help='Имя пользователя')
+        parser.add_argument('-p', '--password', type=str, help='Пароль')
+        parser.add_argument('-e', '--email', type=str, help='Почта')
 
     def handle(self, *args, **kwargs):
-        username = kwargs['-u']
-        password = kwargs['-p']
-        email = kwargs['-e']
+        username = kwargs['user']
+        password = kwargs['password']
+        email = kwargs['email']
         User.objects.create_user(username=username, first_name=username, last_name=username, \
                                  email=email, password=password, is_staff=True)
