59934ba49ae0c22a9b58e30a24d39ce3f1d0cf98
PAN <cool.cyclon@yandex.ru>
2021-11-08 22:12:49 +0300

    lesson 3

--- projects/models.py
+++ projects/models.py
@@ -9,7 +9,7 @@ class Project(models.Model):
 
 
 class ToDo(models.Model):
-    project = models.ForeignKey(Project, name='project', on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     text_todo = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
