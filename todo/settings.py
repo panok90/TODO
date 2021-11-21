6cba0e06c5c11b956f34e25ab755868161161d46
PAN <cool.cyclon@yandex.ru>
2021-11-21 14:49:06 +0300

    finished lesson 8

--- todo/settings.py
+++ todo/settings.py
@@ -134,11 +134,11 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        #'rest_framework.authentication.TokenAuthentication',
     ]
 
 }
